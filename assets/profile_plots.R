6a333e03e2a3b2adfa2d16218df4cdbc3e5c0073
scwatts <s.watts2@student.unimelb.edu.au>
2018-01-22 11:17:08 +1100

    Updated figures, squashed bugs

--- assets/profile_plots.R
+++ assets/profile_plots.R
@@ -38,7 +38,7 @@ d.memory.melt$samples_facet <- name_map.samples[as.character(d.memory.melt$Sampl
   d.time.melt$samples_facet <- factor(d.time.melt$samples_facet, levels=levels)
 }
 {
-  order <- order(d.memory.melt$samples)
+  order <- order(d.memory.melt$Samples)
   levels <- unique(as.character(d.memory.melt$samples_facet[order]))
   d.memory.melt$samples_facet <- factor(d.memory.melt$samples_facet, levels=levels)
 }
@@ -57,7 +57,8 @@ png(filename='plots/comparison_full_time.png', height=1000, width=1400, res=160)
 g <- ggplot(d.time.melt, aes(x=OTUs, y=time, colour=Method)) + geom_line() + facet_wrap(nrow=2, ~ samples_facet)
 g <- g + theme(axis.text.x = element_text(angle=45, hjust=1))
 g <- g + xlim(0, 2500)
-g <- g + labs(x='OTU number', y='Time (minutes)', colour='Method')
+g <- g + scale_colour_manual(values=c("#619CFF", "#00BA38", "#F8766D"))
+g <- g + labs(x='OTU number', y='Wall time (minutes)', colour='Method')
 g
 }
 dev.off()
@@ -66,27 +67,28 @@ png(filename='plots/comparison_full_memory.png', height=1000, width=1400, res=16
 {
 g <- ggplot(d.memory.melt, aes(x=OTUs, y=memory, colour=Method)) + geom_line() + facet_wrap(nrow=2, ~ samples_facet)
 g <- g + xlim(0, 2500)
+g <- g + scale_colour_manual(values=c("#619CFF", "#F8766D"))
 g <- g + theme(axis.text.x = element_text(angle=45, hjust=1))
-g <- g + labs(x='OTU number', y='Time (minutes)', colour='Method')
+g <- g + labs(x='OTU number', y='Wall time (minutes)', colour='Method')
 g
 }
 dev.off()
 
 # Summarised
-png(filename='plots/comparison_summary_time.png', height=1000, width=1400, res=160)
+png(filename='plots/comparison_summary_time.png', height=1080, width=1920, res=220)
 {
   g <- ggplot(d.time.melt, aes(x=factor(OTUs), y=time, colour=Method)) + geom_boxplot(width = 0.25)
-  g <- g + scale_colour_manual(values=c("#F8766D", "#00BA38", "#619CFF"))
-  g <- g + labs(x='OTUs', y='Time (minutes)')
+  g <- g + scale_colour_manual(values=c("#619CFF", "#00BA38", "#F8766D"))
+  g <- g + labs(title='A', x='OTUs', y='Wall time (minutes)')
   g
 }
 dev.off()
 
-png(filename='plots/comparison_summary_memory.png', height=1000, width=1400, res=160)
+png(filename='plots/comparison_summary_memory.png', height=1080, width=1920, res=220)
 {
   g <- ggplot(d.memory.melt, aes(x=factor(OTUs), y=memory, colour=Method)) + geom_boxplot(width = 0.25)
   g <- g + scale_colour_manual(values=c("#619CFF", "#F8766D"))
-  g <- g + labs(x='OTUs', y='Memory (GB)')
+  g <- g + labs(title='B', x='OTUs', y='Memory (GB)')
   g
 }
 dev.off()
