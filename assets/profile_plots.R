227fc4368ed70ba286d7bb02d60a770fe305b6f3
scwatts <s.watts2@student.unimelb.edu.au>
2018-02-05 12:29:28 +1100

    Updated plotting

--- assets/profile_plots.R
+++ assets/profile_plots.R
@@ -46,49 +46,54 @@ d.memory.melt$samples_facet <- name_map.samples[as.character(d.memory.melt$Sampl
 
 # Linear models
 fastspar.memory.lm <- lm(sqrt(d.memory$`FastSpar (single thread)`) ~ d.memory$OTUs + d.memory$Samples)
-fastspar.time.lm <- lm(sqrt(d.time$`FastSpar (16 threads)`) ~ d.time$OTUs + d.time$Samples)
+#fastspar.time.lm <- lm(sqrt(d.time$`FastSpar (16 threads)`) ~ d.time$OTUs + d.time$Samples)
+fastspar.time.lm <- lm(sqrt(d.time$`FastSpar (single thread)`) ~ d.time$OTUs + d.time$Samples)
 summary(fastspar.memory.lm)
 summary(fastspar.time.lm)
 
 
 ### Plot
-png(filename='plots/comparison_full_time.png', height=1000, width=1400, res=160)
+svg(filename='plots/comparison_full_time.svg', height=8, width=16)
 {
-g <- ggplot(d.time.melt, aes(x=OTUs, y=time, colour=Method)) + geom_line() + facet_wrap(nrow=2, ~ samples_facet)
-g <- g + theme(axis.text.x = element_text(angle=45, hjust=1))
-g <- g + xlim(0, 2500)
-g <- g + scale_colour_manual(values=c("#619CFF", "#00BA38", "#F8766D"))
-g <- g + labs(x='OTU number', y='Wall time (minutes)', colour='Method')
-g
-}
-dev.off()
-
-png(filename='plots/comparison_full_memory.png', height=1000, width=1400, res=160)
-{
-g <- ggplot(d.memory.melt, aes(x=OTUs, y=memory, colour=Method)) + geom_line() + facet_wrap(nrow=2, ~ samples_facet)
-g <- g + xlim(0, 2500)
-g <- g + scale_colour_manual(values=c("#619CFF", "#F8766D"))
-g <- g + theme(axis.text.x = element_text(angle=45, hjust=1))
-g <- g + labs(x='OTU number', y='Wall time (minutes)', colour='Method')
-g
-}
-dev.off()
-
-# Summarised
-png(filename='plots/comparison_summary_time.png', height=1080, width=1920, res=220)
-{
-  g <- ggplot(d.time.melt, aes(x=factor(OTUs), y=time, colour=Method)) + geom_boxplot(width = 0.25)
+  g <- ggplot(d.time.melt, aes(x=OTUs, y=time, colour=Method)) + geom_line() + facet_wrap(nrow=2, ~ samples_facet)
+  g <- g + theme(axis.text.x = element_text(angle=45, hjust=1))
+  g <- g + theme(text=element_text(size=18))
+  g <- g + xlim(0, 2500)
   g <- g + scale_colour_manual(values=c("#619CFF", "#00BA38", "#F8766D"))
-  g <- g + labs(title='A', x='OTUs', y='Wall time (minutes)')
+  g <- g + labs(x='OTU number', y='Wall time (minutes)', colour='Method')
   g
 }
 dev.off()
 
-png(filename='plots/comparison_summary_memory.png', height=1080, width=1920, res=220)
+svg(filename='plots/comparison_full_memory.svg', height=8, width=16)
+{
+  g <- ggplot(d.memory.melt, aes(x=OTUs, y=memory, colour=Method)) + geom_line() + facet_wrap(nrow=2, ~ samples_facet)
+  g <- g + xlim(0, 2500)
+  g <- g + scale_colour_manual(values=c("#619CFF", "#F8766D"))
+  g <- g + theme(axis.text.x = element_text(angle=45, hjust=1))
+  g <- g + theme(text=element_text(size=18))
+  g <- g + labs(x='OTU number', y='Wall time (minutes)', colour='Method')
+  g
+}
+dev.off()
+
+# Summarised
+svg(filename='plots/comparison_summary_time.svg', height=8, width=14)
+{
+  g <- ggplot(d.time.melt, aes(x=factor(OTUs), y=time, colour=Method)) + geom_boxplot(width = 0.25)
+  g <- g + scale_colour_manual(values=c("#619CFF", "#00BA38", "#F8766D"))
+  g <- g + theme(text=element_text(size=24))
+  g <- g + labs(x='OTUs', y='Wall time (minutes)')
+  g
+}
+dev.off()
+
+svg(filename='plots/comparison_summary_memory.svg', height=8, width=14)
 {
   g <- ggplot(d.memory.melt, aes(x=factor(OTUs), y=memory, colour=Method)) + geom_boxplot(width = 0.25)
   g <- g + scale_colour_manual(values=c("#619CFF", "#F8766D"))
-  g <- g + labs(title='B', x='OTUs', y='Memory (GB)')
+  g <- g + theme(text=element_text(size=24))
+  g <- g + labs(x='OTUs', y='Memory (GB)')
   g
 }
 dev.off()
